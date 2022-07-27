45bc34f16db9897665d31618795d55b05e6420e8
Clavij05 <101677580+Clavij05@users.noreply.github.com>
2022-07-26 22:06:19 -0300

    Add files via upload

---
+++ miarchivo.js
@@ -0,0 +1,15 @@
+
+
+
+let fechaDia=Number(prompt("ingrese su dia de nacimiento"));
+let fechaMes=Number(prompt("ingrese su mes de nacimiento"));
+let fechaAnio=Number(prompt("ingrese su año de nacimiento"));
+const fechaActualDia=26;
+const fechaActualMes=07;
+const fechaActualAnio=2022;
+
+
+while((fechaAnio<fechaActualAnio)&(fechaMes<fechaActualMes)& (fechaDia<fechaActualDia)){
+
+    alert("Usted es menor de edad, vuelva cuando sea mayor");  
+};
